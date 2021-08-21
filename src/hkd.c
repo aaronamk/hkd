2d1ebb216d089199dd349694908549b5945146fb
aaronamk <amklein@email.wm.edu>
2021-08-21 16:05:19 -0400

    Moved into src dir.

--- src/hkd.c
+++ src/hkd.c
@@ -1,13 +1,20 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <linux/input.h>
+#include <signal.h>
 
 
 /* https://www.kernel.org/doc/html/latest/input/event-codes.html */
 #define INPUT_VAL_PRESS 1
 #define INPUT_VAL_RELEASE 0
 #define INPUT_VAL_REPEAT 2
+
+struct binding {
+	int key;
+	char *cmd;
+};
 
 
 int read_event(struct input_event *event) {
@@ -22,6 +29,12 @@ void write_event(const struct input_event *event) {
 
 
 int main(int argc, char *argv[]) {
+	FILE *cache = fopen(strcat(getenv("XDG_CACHE_HOME"), "/hkd_cache"), "r");
+	char pid[20];
+	fgets(pid, 20, cache);
+	fclose(cache);
+
+	union sigval message;
 	struct input_event input;
 	setbuf(stdin, NULL), setbuf(stdout, NULL);
 	while (read_event(&input)) {
@@ -36,8 +49,16 @@ int main(int argc, char *argv[]) {
 
 		switch (input.value) {
 			case INPUT_VAL_PRESS:
+				if (input.code == KEY_A) {
+					message.sival_int = 1;
+					system("touch YOO");
+					sigqueue(atoi(pid), SIGUSR1, message);
+				}
+				else write_event(&input);
 				break;
 			case INPUT_VAL_RELEASE:
+				if (input.code == KEY_A) {}
+				else write_event(&input);
 				break;
 			case INPUT_VAL_REPEAT:
 				/* linux console, X, wayland handles repeat */
