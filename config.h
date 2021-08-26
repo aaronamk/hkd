b03a0804bae71d3deeb0b78fa467339dd996f86a
aaronamk <amklein@email.wm.edu>
2021-08-26 15:26:40 -0400

    Add media controls.

--- config.h
+++ config.h
@@ -22,12 +22,22 @@ static const int mod_keys[] = { KEY_LEFTSHIFT, KEY_RIGHTSHIFT, KEY_LEFTALT, KEY_
 
 /* commands */
 static const char *shutdown[] = { "shutdown", "now", NULL };
+
+/* requires pulseaudio */
 static const char *vol_up[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+2%", NULL };
 static const char *vol_down[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-2%", NULL };
 static const char *vol_toggle_mute[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
 
+/* requires playerctl */
+static const char *media_next[] = { "playerctl", "--player=playerctld", "next", NULL };
+static const char *media_prev[] = { "playerctl", "--player=playerctld", "previous", NULL };
+static const char *media_forward[] = { "playerctl", "--player=playerctld", "position", "5+", NULL };
+static const char *media_backward[] = { "playerctl", "--player=playerctld", "position", "5-", NULL };
+static const char *media_toggle_pause[] = { "playerctl", "--player=playerctld", "play-pause", NULL };
+
 /* To use envoronment variables or run scripts, add "sh -c" */
 static const char *term[] = { "sh", "-c", "$TERMINAL", NULL };
+static const char *editor[] = { "sh", "-c", "$TERMINAL", "-e", "$EDITOR", NULL };
 /* static const char *shell_script[] = { "sh", "-c", "my_shell_script.sh", NULL }; */
 
 /* bindings */
@@ -37,5 +47,12 @@ static const struct binding bindings[] = {
 	{M_NONE,                KEY_VOLUMEUP,       vol_up},
 	{M_NONE,                KEY_VOLUMEDOWN,     vol_down},
 	{M_NONE,                KEY_MUTE,           vol_toggle_mute},
+	{M_NONE,                KEY_NEXTSONG,       media_next},
+	{M_NONE,                KEY_PREVIOUSSONG,   media_prev},
+	{M_SHIFT,               KEY_NEXTSONG,       media_forward},
+	{M_SHIFT,               KEY_PREVIOUSSONG,   media_backward},
+	{M_NONE,                KEY_PLAYPAUSE,      media_toggle_pause},
+	{M_NONE,                KEY_MUTE,           vol_toggle_mute},
 	{M_META,                KEY_ENTER,          term},
+	{M_META|M_SHIFT,        KEY_ENTER,          editor},
 };
